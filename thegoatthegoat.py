2b42fe18c16bec3653264739f7b1121f119324c1
Shaurya21032014 <haritika.arora@gmail.com>
2026-01-11 15:42:27 +0530

    Add files via upload

---
+++ thegoatthegoat.py
@@ -0,0 +1,29 @@
+def countdown(n):
+    # show the courrent value
+    print("entering function, n=", n)
+
+
+
+
+    # base case 
+    if n == 0:
+        print("reached 0 = stop recurision")
+        return
+    
+
+
+
+    # runs after returnong from recurision
+    print("exiting function, n =", n)
+
+
+
+# main program
+print("program stared\n")
+
+
+
+countdown(5)
+
+
+print("\nProgram finished")
